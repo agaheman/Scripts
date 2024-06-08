879c761913ba4a8f75a73fa8435d26163b77045a
Agah Shamekhi <agah.shamekhi@gmail.com>
2024-06-08 20:45:37 +0330

    Create list-tasks.js

---
+++ list-tasks.js
@@ -0,0 +1,112 @@
+{
+    "tasks": [
+        {
+            "id": "hamster_drop",
+            "rewardCoins": 100000,
+            "periodicity": "Once",
+            "link": "https://hamsterkombat.art",
+            "isCompleted": false
+        },
+        {
+            "id": "watch_youtube_video_okx",
+            "rewardCoins": 50000,
+            "periodicity": "Once",
+            "link": "https://youtu.be/vJPGjGlqmoY",
+            "isCompleted": false
+        },
+        {
+            "id": "subscribe_telegram_channel",
+            "rewardCoins": 5000,
+            "periodicity": "Once",
+            "link": "https://t.me/hamster_kombat",
+            "channelId": -1002075341442,
+            "isCompleted": true,
+            "completedAt": "2024-06-05T12:49:07.791Z"
+        },
+        {
+            "id": "watch_youtube_video_hk_cipher",
+            "rewardCoins": 100000,
+            "periodicity": "Once",
+            "link": "https://youtu.be/sBDvfY1C6Io",
+            "isCompleted": false
+        },
+        {
+            "id": "subscribe_youtube_channel",
+            "rewardCoins": 50000,
+            "periodicity": "Once",
+            "link": "https://www.youtube.com/@HamsterKombat_Official?sub_confirmation=1",
+            "isCompleted": true,
+            "completedAt": "2024-06-05T14:05:27.451Z"
+        },
+        {
+            "id": "subscribe_x_account",
+            "rewardCoins": 5000,
+            "periodicity": "Once",
+            "link": "https://twitter.com/hamster_kombat",
+            "isCompleted": true,
+            "completedAt": "2024-06-05T12:50:11.348Z"
+        },
+        {
+            "id": "select_exchange",
+            "rewardCoins": 5000,
+            "periodicity": "Once",
+            "isCompleted": false
+        },
+        {
+            "id": "invite_friends",
+            "rewardCoins": 25000,
+            "periodicity": "Once",
+            "isCompleted": false
+        },
+        {
+            "id": "streak_days",
+            "rewardCoins": 5000,
+            "periodicity": "Repeatedly",
+            "rewardsByDays": [
+                {
+                    "days": 1,
+                    "rewardCoins": 500
+                },
+                {
+                    "days": 2,
+                    "rewardCoins": 1000
+                },
+                {
+                    "days": 3,
+                    "rewardCoins": 2500
+                },
+                {
+                    "days": 4,
+                    "rewardCoins": 10000000
+                },
+                {
+                    "days": 5,
+                    "rewardCoins": 15000
+                },
+                {
+                    "days": 6,
+                    "rewardCoins": 25000
+                },
+                {
+                    "days": 7,
+                    "rewardCoins": 100000
+                },
+                {
+                    "days": 8,
+                    "rewardCoins": 500000
+                },
+                {
+                    "days": 9,
+                    "rewardCoins": 1000000
+                },
+                {
+                    "days": 10,
+                    "rewardCoins": 5000000
+                }
+            ],
+            "days": 4,
+            "isCompleted": false,
+            "completedAt": "2024-06-07T01:20:10.311Z"
+        }
+    ]
+}
